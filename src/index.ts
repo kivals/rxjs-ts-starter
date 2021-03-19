4e8136927096452c2529c02fe7a5557b4077a7e8
melnik <melnik@bifit.com>
2021-03-19 12:11:32 +0300

    Solution

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
-import { of, from } from 'rxjs';
+import {fromEvent} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map, switchMap} from "rxjs/operators";
+import {ajax} from "rxjs/ajax";
 
-const o = of(5) // Promise.resolve(5)
+type GitHubUsersResponse = {
+  items: Array<GitHubUser>
+}
 
-o.subscribe({
-  next: (value: any) => console.log('Next:', value),
+type GitHubUser = {
+    login: string
+}
+
+const URL = 'https://api.github.com/search/users?q=';
+const search = document.getElementById('search');
+
+const stream$ = fromEvent(search, 'input')
+    .pipe(
+        map(e => {
+          return (e.target as HTMLInputElement).value;
+        }),
+        debounceTime(1000),
+        distinctUntilChanged(),
+        switchMap(v => ajax.getJSON(URL + v)),
+        map((res: GitHubUsersResponse) => res.items),
+    )
+
+stream$.subscribe({
+  next: (value: any) => value.map(u => console.log(u.login)),
   complete: () => console.log('Complete!'),
   error: (error) => console.log('Error!', error)
 })
